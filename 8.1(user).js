d5ba175b92d76771086c654946c5a4bd296015ef
kingurru <kingnikoriginal@gmail.com>
2020-12-01 22:17:25 +0300

    fix 8task

--- 8.1(user).js
+++ 8.1(user).js
@@ -2,7 +2,7 @@
       Дан объект let user = { name: "Ivan", years: 19, car: "Subaru", telePhone: "iPhone", city: "Minsk" }.
 	  Запишите поле name в переменную userName, поле year в переменную userName,
 	  поле car в переменную userCar, остальные поля записать в объект userOtherInfo;*/
-
+Деструктиризация
 let user = { name: "Ivan", years: 19, car: "Subaru", telePhone: "iPhone", city: "Minsk" }
 
 let {name, years, car} = user
