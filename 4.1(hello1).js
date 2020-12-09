801e37df208826c6ae37e74be0b1029f86690588
kingurru <kingnikoriginal@gmail.com>
2020-12-09 22:32:41 +0300

    fix task

--- 4.1(hello1).js
+++ 4.1(hello1).js
@@ -2,7 +2,6 @@
 *   Напишите функцию hello1(), которая при вызове будет возвращать строку «Привет, JavaScript!»
 */
 function hello() {
-    console.log("Привет JavaScript!")
+    return "Привет JavaScript!"
 }
-
 hello()
