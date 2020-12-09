633db11c5e0e1bbd021df7f6b68d6ad2497f9b95
kingurru <kingnikoriginal@gmail.com>
2020-12-09 23:49:55 +0300

    fix task

--- 10.3(function).js
+++ 10.3(function).js
@@ -1,15 +1,18 @@
 /*TODO: task- Создайте функцию, которая позовляет вывести полную информацию о каждом из двух пользователей созданных в предыдущем задании.
 	  Функция должна выполнять вывод информации и не принимать аргументов. По сути нужно одну функцию привязать два объекта.*/
 
-
 function Users(first_name, second_name, age, city) {
     this.first_name = first_name
     this.second_name = second_name
     this.age = age
     this.city = city
+    this.info = function () {
+        console.log(`${this.first_name} ${this.second_name}, ${this.age}, ${this.city}`)
+    }
+
 }
 
 let user1 = new Users("Mikita", "Makarov", 33, "Kiev")
 let user2 = new Users("Vlad", "Vilich", 44, "Moscow")
 
-let info = () => console.log([user1, user2])
+console.log(user1.info(), user2.info())
