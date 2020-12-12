0edc7739fc16290544ed33a0f4fcd4c3f51551c4
kingurru <kingnikoriginal@gmail.com>
2020-12-12 15:59:53 +0300

    end task

--- 11.2(calcArea).js
+++ 11.2(calcArea).js
@@ -8,22 +8,25 @@ function HouseBuilder(width, length, floor) {
     this.length = length
     this.floor = floor
     this.info = function () {
-        console.log(`width: ${this.width}, length: ${this.length}, floor: ${this.floor}`)
+        console.log(`width: ${this.width}, length: ${this.length}, floor: ${this.floor}, area: ${this.totalArea}`)
     }
 }
 
 HouseBuilder.prototype.calcArea = function () {
-    return  +`${this.width * this.length * this.floor}`
-
+    this.totalArea = this.width * this.length * this.floor
+    return this.totalArea
+    // return  this.width * this.length * this.floor
 }
 
-let a100 = new HouseBuilder(300, 400, 15)
-let mapid = new HouseBuilder(200, 250, 11)
+let a100 = new HouseBuilder(3, 4, 15)
+let mapid = new HouseBuilder(2, 5, 11)
 
-a100.totalArea = a100.calcArea()
-mapid.totalArea = mapid.calcArea()
+// a100.totalArea = a100.calcArea()
+// mapid.totalArea = mapid.calcArea()
 
-console.log(a100, mapid)
+a100.calcArea()
+a100.info()
 
-
+mapid.calcArea()
+mapid.info()
 
--- 11.3(askArea).js
+++ 11.3(askArea).js
@@ -11,3 +11,38 @@
 	    askArea(secondHouse.calcArea, secondHouse.checkAreaSuccessfully, secondHouse.checkAreaFail)
     Будет ли такой код работать корректно? Если нет то используйте известные вам механизмы привязки чтобы решить эту задачу.
 */
+function HouseBuilder(width, length, floor) {
+    this.width = width
+    this.length = length
+    this.floor = floor
+    this.info = function () {
+        console.log(`width: ${this.width}, length: ${this.length}, floor: ${this.floor}, area: ${this.totalArea}`)
+    }
+}
+
+HouseBuilder.prototype.calcArea = function () {
+    this.totalArea = this.width * this.length * this.floor
+    return this.totalArea
+}
+
+let a100 = new HouseBuilder(3, 4, 15)
+let mapid = new HouseBuilder(2, 5, 11)
+
+a100.calcArea()
+a100.info()
+
+mapid.calcArea()
+mapid.info()
+
+//todo:3task
+function askArea(result, resolve, reject) {
+    let area = result()
+    if (area >= "120") resolve()
+    else reject()
+}
+
+HouseBuilder.prototype.checkAreaSuccessfully = () => alert("План по стройке выполнен")
+HouseBuilder.prototype.checkAreaFail = () => alert("План по стройке не выполнен")
+
+askArea(a100.calcArea.bind(a100), a100.checkAreaSuccessfully.bind(a100), a100.checkAreaFail.bind(a100))
+askArea(mapid.calcArea.bind(mapid), mapid.checkAreaSuccessfully.bind(mapid), mapid.checkAreaFail.bind(mapid))
